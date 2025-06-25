ed54a1bc64c6575f71d8e0d07ec579ad2eef2de8
Yor Marte <yordymartegacia@gmail.com>
2025-06-24 22:01:40 -0400

    Aquitectura de el projecto

---
+++ backend/api/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { NotesModule } from './notes/notes.module';
+
+@Module({
+  imports: [TasksModule, NotesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
